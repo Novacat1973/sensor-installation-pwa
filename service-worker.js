071948eb0a0785920a516f23dcafc69d307a21b6
Novacat1973 <69090667+Novacat1973@users.noreply.github.com>
2026-03-27 07:23:36 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,24 @@
-const CACHE_NAME = "installer-cache-v1";
+const CACHE_NAME = "installer-cache-v2";
+
 const FILES_TO_CACHE = [
-  "index.html",
-  "jsqr.min.js",
-  "manifest.json",
-  "icon192.png",
-  "icon512.png"
+  "./",
+  "./index.html",
+  "./jsqr.min.js",
+  "./manifest.json",
+  "./icon192.png",
+  "./icon512.png"
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
+self.addEventListener("install", (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => r || fetch(e.request))
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
   );
 });
